22b32bb7c0592b1025c6e97391318cce3ed20749
Luis Caraballo <lecmbogota@gmail.com>
2025-04-14 18:43:22 -0500

    feat: Eliminar configuración de PWA del archivo de configuración de Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,34 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: '/', // Solo si tu aplicación se sirve desde un subdirectorio, ajusta este valor según corresponda.
   plugins: [
-    vue(),
-    VitePWA({
-      registerType: 'autoUpdate', // Permite la actualización automática del Service Worker
-      manifest: {
-        name: 'BodegApp',
-        short_name: 'BodegApp',
-        start_url: '/login', // La URL de inicio al instalar la PWA
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#235ca4',
-        icons: [
-          {
-            src: 'android-launchericon-192-192.png', // Asegúrate de tener estos archivos en la carpeta public.
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'android-launchericon-512-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+    vue()
   ],
   resolve: {
     alias: {
